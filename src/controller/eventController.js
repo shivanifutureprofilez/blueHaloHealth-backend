6fbe788343d000198e2ead4ab84834509c6945c4
shivanijwork <shivanijwork@gmail.com>
2025-11-01 18:05:56 +0530

    events

--- src/controller/eventController.js
+++ src/controller/eventController.js
@@ -44,7 +44,7 @@ exports.addEvent = async (req,res) => {
 
 exports.listEvent = (async (req,res) => {
     try {
-        const list = await Event.find();
+        const list = await Event.find({ deletedAt: null });
         if(!list){
             return res.status(400).json({
                 status:false,
@@ -66,5 +66,66 @@ exports.listEvent = (async (req,res) => {
         })
     }
 });
+
+exports.deleteEvent = (async (req,res) => {
+    try{
+    const _id = req.params.id;
+    const eventData = await Event.findByIdAndUpdate(_id);
+    eventData.deletedAt = new Date();
+    eventData.save();
+    if(!eventData){
+        return res.status(400).json({
+            status:false,
+            message:"Unable To delete Event"
+        })
+    }
+    return res.status(200).json({
+        status:true,
+        message:"Event Deleted Successfully"
+    })
+    }
+    catch(error){
+        console.log("error :",error);
+        res.status(500).json({
+            status:false,
+            message:"Unable To delete Event.Something Went Wrong"
+        })
+    }
+});
+
+exports.updateEvent = (async (req,res) => {
+    try {
+        const _id = req.params.id;
+        const { name, description, link, linkText, startDate, endDate} = req.body;
+        console.log("_id : ",_id);
+        if(!name || ! description || !link || !linkText|| !startDate || !endDate){
+            return res.status(400).json({
+                status:false,
+                message:"All fields required",
+            });
+        }
+        const eventData = await Event.findByIdAndUpdate(_id, { name, description, link, linkText, startDate, endDate});
+        if (!eventData) {
+            return res.status(400).json({
+                status: false,
+                message: "Unable To Update Event",
+                eventData:[]
+            })
+        }
+        return res.status(200).json({
+            status: true,
+            message: "Event Updated Successfully",
+            eventData: eventData
+        })
+    } catch (error) {
+        console.log("error : ",error);
+        return res.status(500).json({
+            status:false,
+            message:"Unable to Update Event. Try Again Later",
+            error:error
+        })
+    }
+});
+
 
 
--- src/routes/eventRoutes.js
+++ src/routes/eventRoutes.js
@@ -1,8 +1,10 @@
-const { addEvent, listEvent } = require("../controller/eventController");
+const { addEvent, listEvent, deleteEvent, updateEvent } = require("../controller/eventController");
 
 const router = require("express").Router();
 
 router.post("/event/add", addEvent);
 router.get("/event/list", listEvent);
+router.get('/event/delete/:id', deleteEvent);
+router.post('/event/update/:id',updateEvent);
 
 module.exports = router;
