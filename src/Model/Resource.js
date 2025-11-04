db63c0dfa11110a7fe01bd230706961d1c2ca198
shivanijwork <shivanijwork@gmail.com>
2025-11-04 17:19:44 +0530

    patient portal button

--- src/Model/Resource.js
+++ src/Model/Resource.js
@@ -5,17 +5,17 @@ const resourceSchema = new mongoose.Schema({
         type: String,
         required: [true, "Resource title is required"]
     },
-    date: {
-        type: Date,
-        required: [true, "Date is required"]
-    },
-    tags: {
-        type: String,
-        required: [true, "Resource Tags are Required"]
-    },
     link: {
         type: String,
         required: [true, "Resource Link is Required"]
+    },
+    tags:  {
+        type: [String],
+        required: [true, "Resource Tags are Required"]
+    },
+    date: {
+        type: Date,
+        required: [true, "Date is required"]
     },
     deletedAt: {
         type: Date,
