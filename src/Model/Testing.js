bcb36a8d54274254d610fd6c829b20016e6362ce
shivanijwork <shivanijwork@gmail.com>
2025-11-10 18:20:08 +0530

    about section

---
+++ src/Model/Testing.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const testingSchema = new mongoose.Schema(
+    {
+        name:{
+            type:String,
+            required:[true, "Name is required"],
+        },
+        description:{
+            type:String,
+            required:[true, "Description is Required"],
+        },
+        link:{
+            type:String,
+            required:[true, "Event Link is Required"],
+        },
+        linkText:{
+            type:String,
+            required:[true,"Event Link Text is required"],
+        },
+        startDate:{
+            type:Date,
+            required:[true, "Start Date is Required"],
+        },
+        endDate:{
+            type:Date,
+            required:[true, "End Date is Required"],
+        },
+        deletedAt:{
+            type:Date,
+            default:null,
+        },
+    },
+    {timestamp: true},
+);
+
+const Testing = mongoose.model('testings',testingSchema);
+module.exports = Testing;
--- src/routes/eventRoutes.js
+++ src/routes/eventRoutes.js
@@ -1,10 +1,13 @@
 const { addEvent, listEvent, deleteEvent, updateEvent } = require("../controller/eventController");
+const { addTesting } = require("../controller/testingController");
 
 const router = require("express").Router();
 
 router.post("/event/add", addEvent);
+router.post("/testing/add", addTesting);
 router.get("/event/list", listEvent);
 router.get('/event/delete/:id', deleteEvent);
 router.post('/event/update/:id',updateEvent);
+
 
 module.exports = router;
