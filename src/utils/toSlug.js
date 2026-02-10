e4128cdcfbe8cf2d532f6330aa0c24c619e49313
shivanijwork <shivanijwork@gmail.com>
2026-02-10 16:24:45 +0530

    slug

--- src/utils/toSlug.js
+++ src/utils/toSlug.js
@@ -1,16 +1,19 @@
-export const toSlug = (str) => {
-        console.log("slug str ",str);
-    if (str) {
-        console.log("str : ",str);
-      return str
-        .toLowerCase()
-        .trim()
-        .normalize("NFD")                 // split accented letters
-        .replace(/[\u0300-\u036f]/g, "")  // remove accents
-        .replace(/[^a-z0-9]+/g, "-")      // remove non-alphanumerics
-        .replace(/^-+|-+$/g, "");         // trim hyphens
-    } else {
-      return ''
-    }
-  };
+const toSlug = (str) => {
+  console.log("slug str ", str);
 
+  if (str) {
+    return str
+      .toLowerCase()
+      .trim()
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "")
+      .replace(/[^a-z0-9]+/g, "-")
+      .replace(/^-+|-+$/g, "");
+  } else {
+    return "";
+  }
+};
+
+module.exports = {
+  toSlug
+};
